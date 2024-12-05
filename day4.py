bccf1fa745fa1ee47b35d59a096c038987f56c03
rocket3989 <rocket3989@gmail.com>
2024-12-04 21:18:38 -0800

    actually add day5, update matrix logic for day4

--- day4.py
+++ day4.py
@@ -1,7 +1,7 @@
 from aocd import get_data
 from rich import print
 from collections import defaultdict, Counter
-
+import regex as re
 data = get_data(day=4, year=2024)
 
 test_data ='''MMMSXXMASM
@@ -17,9 +17,12 @@ MXMXAXMASX'''
 
 
 def solve1(data: str) -> int:
-    matrix = data.split("\n")
-    n, m = len(matrix), len(matrix[0])
-    matrix = [[matrix[i][j] for j in range(m)] for i in range(n)]
+    matrix_data = data.split("\n")
+    n, m = len(matrix_data), len(matrix_data[0])
+    matrix = defaultdict(lambda: defaultdict(str))
+    for i in range(n):
+        for j in range(m):
+            matrix[i][j] = matrix_data[i][j]
     ans = 0
 
 
@@ -28,11 +31,7 @@ def solve1(data: str) -> int:
             for dir in [(-1, 0), (1, 0), (0, -1), (0, 1), (-1, -1), (-1, 1), (1, -1), (1, 1)]:
 
                 for pos, char in enumerate("XMAS"):
-                    if (i + dir[0] * pos < 0 or 
-                        j + dir[1] * pos < 0 or 
-                        i + dir[0] * pos >= n or 
-                        j + dir[1] * pos >= m or
-                        matrix[i + dir[0] * pos][j + dir[1] * pos] != char):
+                    if matrix[i + dir[0] * pos][j + dir[1] * pos] != char:
                         break
                 else:
                     ans += 1
@@ -41,9 +40,13 @@ def solve1(data: str) -> int:
     return ans
 
 def solve2(data: str) -> int:
-    matrix = data.split("\n")
-    n, m = len(matrix), len(matrix[0])
-    matrix = [[matrix[i][j] for j in range(m)] for i in range(n)]
+    matrix_data = data.split("\n")
+    n, m = len(matrix_data), len(matrix_data[0])
+    matrix = defaultdict(lambda: defaultdict(str))
+    for i in range(n):
+        for j in range(m):
+            matrix[i][j] = matrix_data[i][j]
+
 
     a_loc = defaultdict(int)
 
@@ -51,18 +54,34 @@ def solve2(data: str) -> int:
         for j in range(m):
             for dir in [(-1, -1), (-1, 1), (1, -1), (1, 1)]:
                 for pos, char in enumerate("MAS"):
-                    if (i + dir[0] * pos < 0 or 
-                        j + dir[1] * pos < 0 or 
-                        i + dir[0] * pos >= n or 
-                        j + dir[1] * pos >= m or
-                        matrix[i + dir[0] * pos][j + dir[1] * pos] != char):
+                    if matrix[i + dir[0] * pos][j + dir[1] * pos] != char:
                         break
                 else:
                     a_loc[(i + dir[0], j + dir[1])] += 1
     
     return Counter(a_loc.values())[2]
                 
-                
+def solve1regex(data: str) -> int:
+    matrix = data.split("\n")
+    _, m = len(matrix), len(matrix[0])
+
+    regex = '(XMAS)|(SAMX)'
+    print(m)
+
+
+    for dist in [m - 1, m, m + 1]:
+
+        regex += f'|(X.{{{dist}}}M.{{{dist}}}A.{{{dist}}}S)'
+        regex += f'|(S.{{{dist}}}A.{{{dist}}}M.{{{dist}}}X)'
+    
+    # regex = '(?=(' + regex + '))'
+    print(regex)
+    for match in re.findall(regex, '|'.join(matrix), overlapped=True):
+        print(match)
+    return len(re.findall(regex, '|'.join(matrix), overlapped=True))
+
+
+
 print(solve1(test_data))
 print(solve1(data))
 print(solve2(test_data))
--- day5.py
+++ day5.py
@@ -1,90 +1,104 @@
 from aocd import get_data
-from parse import parse, findall, search
+from parse import findall
 from rich import print
-from collections import defaultdict, deque, Counter
+from collections import defaultdict
 import functools
-from heapq import heappop, heappush
-from itertools import product
-import re
 
-data = get_data(day=4, year=2024)
+data = get_data(day=5, year=2024)
 
 
-test_data ='''MMMSXXMASM
-MSAMXMSMSA
-AMXSXMAAMM
-MSAMASMSMX
-XMASAMXAMM
-XXAMMXXAMA
-SMSMSASXSS
-SAXAMASAAA
-MAMMMXMMMM
-MXMXAXMASX'''
+test_data ='''47|53
+97|13
+97|61
+97|47
+75|29
+61|13
+75|53
+29|13
+97|29
+53|29
+61|53
+97|53
+61|29
+47|13
+75|47
+97|75
+47|61
+75|61
+47|29
+75|13
+53|13
+
+75,47,61,53,29
+97,61,53,29,13
+75,29,13
+75,97,47,61,53
+61,13,29
+97,13,75,29,47'''
 
 
 def solve1(data: str) -> int:
-    # first_line = search("{}\n", data)[0]
-    # ns = [int(x) for x in data.split('\n')]
-    # parsed = [(tuple(r)) for r in findall("mul({:d},{:d})", data)]
-
-    matrix = data.split("\n")
-    n, m = len(matrix), len(matrix[0])
-    matrix = [[matrix[i][j] for j in range(m)] for i in range(n)]
     ans = 0
+    data2 = data.split("\n\n")[1]
 
+    rules = [(tuple(r)) for r in findall("{:d}|{:d}", data)]
+    wrong = defaultdict(set)
 
-    for i in range(n):
-        for j in range(m):
-            if matrix[i][j] != "X":
+    for a, b in rules:
+        wrong[b].add(a)
+
+    for line in data2.split("\n"):
+        line = [int(x) for x in line.split(",")]
+        
+        bad = False
+        for i, val in enumerate(line):
+            for val2 in line[i + 1:]:
+                if val2 in wrong[val]:
+                    bad = True
+                    break
+            else:
                 continue
-
-            for dir in [(-1, 0), (1, 0), (0, -1), (0, 1), (-1, -1), (-1, 1), (1, -1), (1, 1)]:
-                try:
-                    for pos, char in enumerate("MAS", 1):
-                        if i + dir[0] * pos < 0 or j + dir[1] * pos < 0 or matrix[i + dir[0] * pos][j + dir[1] * pos] != char:
-                            break
-                    else:
-                        ans += 1
-
-                except:
-                    continue
-
-
+            break
+        
+        if not bad:
+            ans += line[len(line) // 2]
 
     return ans
 
 def solve2(data: str) -> int:
+
     ans = 0
+    data2 = data.split("\n\n")[1]
+    rules = [(tuple(r)) for r in findall("{:d}|{:d}", data)]
 
-    matrix = data.split("\n")
-    n, m = len(matrix), len(matrix[0])
-    matrix = [[matrix[i][j] for j in range(m)] for i in range(n)]
-    ans = 0
+    wrong = defaultdict(set)
 
+    for a, b in rules:
+        wrong[b].add(a)
 
-    aLoc = defaultdict(int)
-
-
-    for i in range(n):
-        for j in range(m):
-            if matrix[i][j] != "M":
+    for line in data2.split("\n"):
+        line = [int(x) for x in line.split(",")]
+        
+        bad = False
+        for i, val in enumerate(line):
+            for val2 in line[i + 1:]:
+                if val2 in wrong[val]:
+                    bad = True
+                    break
+            else:
                 continue
+            break
+        
+        if not bad:
+            continue
 
-            for dir in [(-1, -1), (-1, 1), (1, -1), (1, 1)]:
-                try:
-                    for pos, char in enumerate("AS", 1):
-                        if i + dir[0] * pos < 0 or j + dir[1] * pos < 0 or matrix[i + dir[0] * pos][j + dir[1] * pos] != char:
-                            break
-                    else:
-                        aLoc[(i + dir[0], j + dir[1])] += 1
+        
+        line.sort(key=functools.cmp_to_key(lambda x, y: -1 if y in wrong[x] else 1))
+        ans += line[len(line) // 2]
 
-                except:
-                    continue
-    
-    # print(aLoc)
-    ans = Counter(aLoc.values())[2] + Counter(aLoc.values())[3]
+        
     return ans
-                
+
 print(solve1(test_data))
 print(solve1(data))
 print(solve2(test_data))
