54db6512f0f84d8629f90e129725b63f1f80e159
rocket3989 <rocket3989@gmail.com>
2024-12-13 23:07:51 -0800

    clean day 14

--- day14.py
+++ day14.py
@@ -1,22 +1,9 @@
 import json
 from aocd import get_data
-from parse import parse, findall, search
+from parse import findall
 from rich import print as print
-from collections import defaultdict, deque, Counter
-import functools
-from heapq import heappop, heappush
-from itertools import permutations, product
-import re
-# from tqdm import tqdm      
-import resource
-import sys
-# from z3 import Int, Optimize, sat, Solver
-
-resource.setrlimit(resource.RLIMIT_STACK, (2**29,-1))
-sys.setrecursionlimit(10**6)
 
 data = get_data(day=14, year=2024)
-
 
 test_data ='''p=0,4 v=3,-3
 p=6,3 v=-1,-3
@@ -33,16 +20,7 @@ p=9,5 v=-3,-3'''
 
 
 def solve1(data: str) -> int:
-    # first_line = search("{}\n", data)[0]
-    # ns = [int(x) for x in data.split('\n')]
     parsed = [(tuple(r)) for r in findall("p={:d},{:d} v={:d},{:d}", data)]
-
-    # matrix_data = data.split("\n")
-    # n, m = len(matrix_data), len(matrix_data[0])
-    # matrix = defaultdict(lambda: defaultdict(str))
-    # for i in range(n):
-    #     for j in range(m):
-    #         matrix[i][j] = matrix_data[i][j]
 
     robots = []
     for row in parsed:
